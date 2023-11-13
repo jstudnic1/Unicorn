0b8d71c82a5e66a2b56e54d6376ac2e826c432fd
jstudnic1 <studnicka.jakub04@gmail.com>
2023-11-13 08:42:52 +0100

    sv

--- 1sem/ukol3.js
+++ 1sem/ukol3.js
@@ -6,7 +6,7 @@ function getRandomGender() {
 }
 
 function getRandomWorkload() {
-	const workload = [10, 20, 30, 40];
+	const workload = [10,20,30,40];
 	return getRandomItem(workload);
 }
 
@@ -26,17 +26,17 @@ function getRandomBirthdate(min, max) {
 	return new Date(randomYear, randomMonth - 1, randomDay).toISOString();
 }
 
-function getRandomEmployee(gender, age) {
+function getRandomEmployee (gender, age) {
 	const maleNames = ["Jakub", "Tomas", "Matej", "Jan", "David", "Martin", "Lukas", "Filip", "Adam", "Vojtech", "Jiri", "Karel", "Michal", "Petr", "Pavel", "Ondrej", "Dominik", "Daniel", "Jakob", "Marek", "Oscar", "Antonin", "Radek", "Rudolf", "Viktor", "Josef", "Benjamin", "Patrik", "Svatoslav", "Tadeas", "Libor", "Miroslav", "Vladimir", "Stanislav", "Eduard", "Igor", "Robert", "Jaroslav", "Hugo", "Dalibor", "Bohumil", "Richard", "Oliver", "Sebastian", "Alan", "Ladislav", "Ales"];
 	const femaleNames = ["Adéla", "Barbora", "Cecílie", "Denisa", "Eliška", "Františka", "Gabriela", "Hana", "Irena", "Jana", "Karolína", "Lucie", "Magdaléna", "Nela", "Olga", "Petra", "Radka", "Šárka", "Tereza", "Uršula", "Věra", "Wanda", "Xénie", "Yvona", "Zdeňka", "Alena", "Božena", "Čestmíra", "Diana", "Emílie", "Flóra", "Gita", "Helena", "Iva", "Jitka", "Klára", "Lenka", "Marie", "Natálie", "Otilie", "Pavlína", "Qiana", "Renáta", "Simona", "Tamara", "Urszula", "Vlasta", "Wlasta"];
-	const maleSurnames = ["Novák", "Svoboda", "Novotný", "Dvořák", "Černý", "Procházka", "Kučera", "Veselý", "Horák", "Němec", "Pospíšil", "Marek", "Hájek", "Král", "Janda", "Jelínek", "Kříž", "Vlček", "Bartoš", "Polák", "Bílý", "Toman", "Štěpánek", "Dušek", "Fiala", "Růžička", "Pavelka", "Kolář", "Čech", "Bureš", "Urban", "Havel", "Kopecký", "Hruška", "Konečný", "Petržela", "Vávra", "Havlíček", "Holub", "Šimůnek", "Tomášek", "Koudelka", "Blažek", "Matoušek", "Linhart", "Moravec", "Kadlec"];
-	const femaleSurnames = ["Kerdová", "Novotná", "Nováková", "Svobodová", "Dvořáková", "Černá", "Procházková", "Kučerová", "Veselá", "Horáková", "Bartošová", "Marešová", "Kratochvílová", "Němcová", "Štěpánová", "Hájková", "Růžičková", "Pospíšilová", "Šimonová", "Kolářová", "Urbanová", "Bláhová", "Fialová", "Zemanová", "Richterová", "Křížová", "Dušková", "Holubová", "Benešová", "Jarošová", "Kopecká", "Machová", "Fialová", "Blažková", "Havlová", "Vlčková", "Jelínková", "Koubková", "Tichá", "Kasalová", "Vrbová", "Moravcová", "Sýkorová", "Pospíchalová", "Dlouhá", "Zábranská", "Burešová", "Sedláčková", "Váchová", "Jílková"];
+	const maleSurnames = ["Novotný", "Bartoň"];
+	const femaleSurnames = ["Kerdová", "Novotná","Nováková", "Svobodová", "Dvořáková", "Černá", "Procházková", "Kučerová", "Veselá", "Horáková", "Bartošová", "Marešová", "Kratochvílová", "Němcová", "Štěpánová", "Hájková", "Růžičková", "Pospíšilová", "Šimonová", "Kolářová", "Urbanová", "Bláhová", "Fialová", "Zemanová", "Richterová", "Křížová", "Dušková", "Holubová", "Benešová", "Jarošová", "Kopecká", "Machová", "Fialová", "Blažková", "Havlová", "Vlčková", "Jelínková", "Koubková", "Tichá", "Kasalová", "Vrbová", "Moravcová", "Sýkorová", "Pospíchalová", "Dlouhá", "Zábranská", "Burešová", "Sedláčková", "Váchová", "Jílková"];
 
 	const birthday = getRandomBirthdate(age.min, age.max);
 	const nameArray = gender === "male" ? maleNames : femaleNames;
 	const surnameArray = gender === "male" ? maleSurnames : femaleSurnames;
 
-	return { gender, birthday, name: getRandomItem(nameArray), surname: getRandomItem(surnameArray), workload: getRandomWorkload() };
+	return {gender, birthday, name: getRandomItem(nameArray), surname: getRandomItem(surnameArray), workload: getRandomWorkload() };
 }
 
 function main(dtoIn) {
@@ -47,8 +47,8 @@ function main(dtoIn) {
 const dtoIn = {
 	count: 50,
 	age: {
-		min: 19,
-		max: 35
+	  min: 19,
+	  max: 35
 	}
 };
 
